a4c79f57fd16fee9792652e48556ebc55d0e712d
pedrold10 <pedrogabriel12423@gmail.com>
2023-06-28 12:03:45 -0300

    Componente de criação de livros concluído

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarLivrosComponent } from './components/listar-livros/listar-livros.component';
+import { AdicionarLivroComponent } from './components/adicionar-livro/adicionar-livro.component';
 
 const routes: Routes = [
   {
     path: 'livros',
     component: ListarLivrosComponent
+  },
+  {
+    path: 'new',
+    component: AdicionarLivroComponent
   },
 ];
 
--- frontend/src/app/components/listar-livros/listar-livros.component.ts
+++ frontend/src/app/components/listar-livros/listar-livros.component.ts
@@ -9,29 +9,6 @@ import { LivrosService } from 'src/app/services/livros.service';
 })
 export class ListarLivrosComponent {
   livros: Livro[] = [
-    {
-      "id": 1,
-      "capa": "https://m.media-amazon.com/images/I/41897yAI4LL._SY344_BO1,204,203,200_QL70_ML2_.jpg",
-      "autor": "J.K. Rowling",
-      "titulo": "Harry Potter and the Sorcerer's Stone",
-      "descricao": "A young boy discovers he is a wizard and embarks on a magical adventure."
-    },
-    {
-      "id": 2,
-      "capa": "https://http2.mlstatic.com/D_NQ_NP_775158-MLU50596264915_072022-O.webp",
-      "autor": "George Orwell",
-      "titulo": "1984",
-      "descricao": "A dystopian novel set in a totalitarian society where individualism is suppressed."
-    },
-    {
-      "id": 3,
-      "capa": "https://papelariacriativa.com.br/wp-content/uploads/livro-literatura-orgulho-e-preconceito-editora-vitrola-1.jpg",
-      "autor": "Jane Austen",
-      "titulo": "Pride and Prejudice",
-      "descricao": "A classic novel exploring themes of love, marriage, and social class in 19th-century England."
-    }
-    
-    
   ];
   
   constructor(private livrosService: LivrosService){}
